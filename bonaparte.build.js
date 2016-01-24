0700db96026167262b1481c902a0bd881e1be083
Philipp Adrian <philipp.adrian@dowjones.com>
2016-01-23 21:35:03 -0500

    remove components -> just css/less/sass: first "bonaparte-theme"

--- bonaparte.build.js
+++ bonaparte.build.js
@@ -3,15 +3,6 @@ require("./src/wsj-fonts.css");
 
 // Bonaparte //////////////////////////////////////////////////////////////////
 
-
-require("bonaparte-panel/bonaparte.build");
-require("bonaparte-scroll/bonaparte.build");
-require("bonaparte-button/bonaparte.build");
-require("bonaparte-sidebar/bonaparte.build");
-require("bonaparte-toolbar/bonaparte.build");
-require("bonaparte-draggable/bonaparte.build");
-require("bonaparte-dropdown/bonaparte.build");
-
 require("./bonaparte.less");
 
 // SUI ////////////////////////////////////////////////////////////////////////
