a92ce9d5ab8432cbe03fee23c9a86d98e0d5d4dd
Philipp Adrian <philipp.adrian@dowjones.com>
2016-01-30 18:19:52 -0500

    add whitney kitchen-sink to examples

---
+++ examples/kitchen-sink whitney/Kitchen-Sink _ Bonaparte_files/bonaparte.js
@@ -0,0 +1,181 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ((function(modules) {
+	// Check all modules for deduplicated modules
+	for(var i in modules) {
+		if(Object.prototype.hasOwnProperty.call(modules, i)) {
+			switch(typeof modules[i]) {
+			case "function": break;
+			case "object":
+				// Module can be created from a template
+				modules[i] = (function(_m) {
+					var args = _m.slice(1), fn = modules[_m[0]];
+					return function (a,b,c) {
+						fn.apply(this, [a,b,c].concat(args));
+					};
+				}(modules[i]));
+				break;
+			default:
+				// Module is a copy of another module
+				modules[i] = modules[modules[i]];
+				break;
+			}
+		}
+	}
+	return modules;
+}({
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	__webpack_require__(50);
+
+
+	// Bonaparte //////////////////////////////////////////////////////////////////
+
+	__webpack_require__(51);
+
+	// SUI ////////////////////////////////////////////////////////////////////////
+
+
+	// require("./src/less/normalize.less"); // require("./SUI/definitions/globals/reset.less");
+	// require("./src/less/base.less"); // require("./SUI/definitions/globals/site.less");
+	// require("./src/less/block.less");
+
+	/* Elements */
+
+	// require("./src/less/button.less"); // require("./SUI/definitions/elements/button.less");
+	// require("./src/less/container.less"); // require("./SUI/definitions/elements/container.less");
+	__webpack_require__(54);
+	// require("./SUI/definitions/elements/header.less");
+	// require("./src/less/header.less");
+	__webpack_require__(55);
+	__webpack_require__(57);
+	// require("./SUI/definitions/elements/image.less");
+	__webpack_require__(66);
+	__webpack_require__(67);
+	// require("./SUI/definitions/elements/list.less");
+	// require("./SUI/definitions/elements/loader.less");
+	// require("./SUI/definitions/elements/rail.less");
+	// require("./SUI/definitions/elements/reveal.less");
+	__webpack_require__(68);
+	// require("./SUI/definitions/elements/step.less");
+
+	/* Collections */
+	// require("./SUI/definitions/collections/breadcrumb.less");
+	__webpack_require__(69);
+	__webpack_require__(70);
+	// require("./SUI/definitions/collections/menu.less");
+	// require("./SUI/definitions/collections/message.less");
+	__webpack_require__(71);
+
+
+	/* Views */
+	// require("./SUI/definitions/views/ad.less");
+	// require("./SUI/definitions/views/card.less");
+	// require("./SUI/definitions/views/comment.less");
+	// require("./SUI/definitions/views/feed.less");
+	// require("./SUI/definitions/views/item.less");
+	// require("./SUI/definitions/views/statistic.less");
+
+	/* Modules */
+	// require("./SUI/definitions/modules/accordion.less");
+	// require("./SUI/definitions/modules/checkbox.less");
+	// require("./SUI/definitions/modules/dimmer.less");
+	// require("./SUI/definitions/modules/dropdown.less");
+	// require("./SUI/definitions/modules/embed.less");
+	// require("./SUI/definitions/modules/modal.less");
+	// require("./SUI/definitions/modules/nag.less");
+	// require("./SUI/definitions/modules/popup.less");
+	// require("./SUI/definitions/modules/progress.less");
+	// require("./SUI/definitions/modules/rating.less");
+	// require("./SUI/definitions/modules/search.less");
+	// require("./SUI/definitions/modules/shape.less");
+	// require("./SUI/definitions/modules/sidebar.less");
+	// require("./SUI/definitions/modules/sticky.less");
+	// require("./SUI/definitions/modules/tab.less");
+	// require("./SUI/definitions/modules/transition.less");
+
+
+
+
+/***/ },
+
+/***/ 50:
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+
+/***/ 51:
+50,
+
+/***/ 54:
+50,
+
+/***/ 55:
+50,
+
+/***/ 57:
+50,
+
+/***/ 66:
+50,
+
+/***/ 67:
+50,
+
+/***/ 68:
+50,
+
+/***/ 69:
+50,
+
+/***/ 70:
+50,
+
+/***/ 71:
+50
+
+/******/ })));
